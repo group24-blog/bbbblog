ffb10982d842becd90b2e0a1cb7a558053e8c5aa
xin <857091490@qq.com>
2020-07-01 23:53:06 +0800

    用于详情页面测试

--- 前端/blog/src/components/test/test.vue
+++ 前端/blog/src/components/test/test.vue
@@ -1,5 +1,37 @@
 <template>
-	<div class="">跳转成功</div>
+	<div class="">
+		<el-container>
+		  <el-header><el-page-header @back="goBack"></el-page-header></el-header>
+		  <el-container>
+		    <el-aside width="200px">
+				<el-card>
+					<div>
+						<el-avatar :src="pic"></el-avatar>
+						<el-button size="small" type="danger">关注</el-button>
+					</div>
+					<div>nobody</div>
+				</el-card>
+			</el-aside>
+		    <el-container>
+		      <el-main><el-card class="content">
+			  <div>Title</div>
+			  <el-divider></el-divider>
+			  <div>Content</div>
+			  </el-card>
+			  </el-main>
+		      <el-footer><!--评论区-->
+						<!--分页page-size每页10条-->
+						<el-pagination
+						  small
+						  :hide-on-single-page="true"
+						  layout="prev, pager, next"
+						  :page-size="10"
+						  :total="50">
+						</el-pagination></el-footer>
+		    </el-container>
+		  </el-container>
+		</el-container>
+	</div>
 </template>
 
 <script>
